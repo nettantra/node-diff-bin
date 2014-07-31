1c8dcc5dfbc073f7b7b46c825c9ac98913a0a50e
devadattas <devadattas@gmail.com>
2014-07-31 17:00:46 +0530

    Added Mocha Tests

--- test/index.js
+++ test/index.js
@@ -1,9 +1,20 @@
-var expect = require('expect.js'),
-    diffBin = require('..');
+var expect = require('expect.js');
+var sh = require('execSync');
+var grunt = require('grunt');
 
-describe('node-diff-bin', function() {
-  it('should say hello', function(done) {
-    expect(diffBin()).to.equal('Hello, world');
+describe('node-diff', function() {
+  it('should print correct patch output', function(done) {
+    var result = sh.exec('node bin/node-diff.js test/fixtures/first.file test/fixtures/second.file');
+    var resultPatch = result.stdout;
+    var expectedPatch = grunt.file.read('test/expected/first-second.patch.out');
+    expect(resultPatch).to.equal(expectedPatch);
+    done();
+  });
+  it('should write correctly to the patch output file', function(done) {
+    var result = sh.exec('node bin/node-diff.js test/fixtures/first.file test/fixtures/second.file tmp/first-second.patch');
+    var resultPatch = grunt.file.read('tmp/first-second.patch');
+    var expectedPatch = grunt.file.read('test/expected/first-second.patch');
+    expect(resultPatch).to.equal(expectedPatch);
     done();
   });
 });
